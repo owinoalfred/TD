e1b3216f994b69866cdeaebb170427e9a5f3b986
alfred <59254825+owinoalfred@users.noreply.github.com>
2021-06-24 14:51:32 +0300

    adding a menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,32 @@
 <template>
-<div class="min-w-30">
-  <Upper />
-  <Navy />
-  <Bodydata />
-  <Lowerbody />
-  <Footer />
+  <div class="min-w-30">
+    <Upper />
+    <Nav />
+    <Navy />
+    <Bodydata />
+    <Lowerbody />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Bodydata from '@/components/Bodydata.vue'
-import Footer from '@/components/Footer.vue'
-import Lowerbody from '@/components/Lowerbody.vue'
-import Navy from '@/components/Navy.vue'
-import Upper from '@/components/Upper.vue'
-
-
+import Bodydata from "@/components/Bodydata.vue";
+import Footer from "@/components/Footer.vue";
+import Lowerbody from "@/components/Lowerbody.vue";
+import Navy from "@/components/Navy.vue";
+import Upper from "@/components/Upper.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Bodydata,
+    Nav,
     Footer,
     Navy,
     Lowerbody,
-    Upper
-
-  }
-}
+    Upper,
+  },
+};
 </script>
