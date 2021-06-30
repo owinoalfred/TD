7997152b7b76c11ffbd4218a13ebb9c5ee166926
alfred <59254825+owinoalfred@users.noreply.github.com>
2021-06-30 23:26:39 +0300

    cpr

--- src/components/Down.vue
+++ src/components/Down.vue
@@ -1,10 +1,14 @@
 <template>
-<div class="w-screen h-20  bg-gray-800">
+<div class="w-screen h-20 text-white bg-gray-800">
+<div class="ml-2">
 
+®alfred Production <br>
+©2021
+</div>
 </div>
 </template>
 <script>
  export default{
 
     }
-</script>
+</script>©®
