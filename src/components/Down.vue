2270b1e9559f75209ed9ed26082a3d2e50d0480b
alfred <59254825+owinoalfred@users.noreply.github.com>
2021-07-04 18:07:48 +0300

    footer

--- src/components/Down.vue
+++ src/components/Down.vue
@@ -1,12 +1,65 @@
 <template>
-<div class="w-screen  text-white bg-gray-800">
-<div class="ml-2">
+<div class=" w-screen  text-white bg-black">
+    <div class="ml-1">
 
-®alfred Production <br>
-©2021
 
+<div >
+    <h1>
+    TECHY-GUY
+    </h1>
+    <p>
+        We create possibilities for a better web look
+    </p>
+    <h3 class="">
+        stand out
+    </h3>
+ </div>
+<br />
+<hr>
+<br />
+<div class="flex">
+ <div>
+     <h3>
+         Explore
+     </h3>
+     <ul>
+         <li><a href="/">Home</a></li>
+         <li><a href="/Pricing">Prices</a></li>
+         <li><a href="/Promos">Promos</a></li>
+         <li><a href="/About">About</a></li>
+
+     </ul>
+ </div>
+ <div class="ml-20">
+     <h3>
+         Contact us
+     </h3>
+     <ul>
+         <li><a href=""><img class='h-5' src="../../dist/img/facebook.png" alt=""></a></li>
+         <li><a href=""><img class='h-5' src="../../dist/img/twitter.png" alt=""></a></li>
+         <li><a href=""><img class='h-5' src="../../dist/img/whatsapp.png" alt=""></a></li>
+     </ul>
+ </div>
+</div>
+<br />
+<hr>
+<br />
+<div>
+<h1>
+    ideas
+</h1>
+
+<h3>
+    propose ideas
+</h3>
+<h3>
+   help translate this page to your Language
+</h3>
 </div>
 </div>
+</div>
+
+
 </template>
 <script>
  export default{
